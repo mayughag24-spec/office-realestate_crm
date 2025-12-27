f3f63e6fb41e1b4f086425c4bcd084a6136d2979
mayughag24-spec <mayughag24@gmail.com>
2025-12-27 11:41:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("index.html")
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/leads/add")
+def add_lead():
+    return render_template("add_lead.html")
+
+@app.route("/leads/view")
+def view_leads():
+    return render_template("view_leads.html")
+
+@app.route("/customers")
+def customers():
+    return render_template("customers.html")
+
+@app.route("/payments")
+def payments():
+    return render_template("payments.html")
 
 if __name__ == "__main__":
     app.run()
